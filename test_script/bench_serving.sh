0ba154fa6fd583f9a6b8e3a6ac32f7f1ebda6ef0
yipin <1259478761@qq.com>
2025-11-05 16:56:37 +0000

    add qwen bench

--- test_script/bench_serving.sh
+++ test_script/bench_serving.sh
@@ -19,17 +19,34 @@
 #     --tokenizer-path OpenGVLab/InternVL3_5-8B-Flash
 
 
+# evalscope perf \
+#     --parallel 64 \
+#     --number 500 \
+#     --model OpenGVLab/InternVL3_5-8B-Flash \
+#     --url http://0.0.0.0:23333/v1/chat/completions \
+#     --api openai \
+#     --dataset random \
+#     --max-tokens 64 \
+#     --min-tokens 64 \
+#     --prefix-length 0 \
+#     --min-prompt-length 600 \
+#     --max-prompt-length 600 \
+#     --tokenizer-path OpenGVLab/InternVL3_5-8B-Flash \
+#     --extra-args '{"ignore_eos": true}'
+
+
 evalscope perf \
     --parallel 64 \
     --number 500 \
-    --model OpenGVLab/InternVL3_5-8B-Flash \
+    --model Qwen/Qwen3-1.7B-FP8 \
     --url http://0.0.0.0:23333/v1/chat/completions \
     --api openai \
     --dataset random \
-    --max-tokens 64 \
-    --min-tokens 64 \
+    --max-tokens 270 \
+    --min-tokens 270 \
     --prefix-length 0 \
-    --min-prompt-length 600 \
-    --max-prompt-length 600 \
-    --tokenizer-path OpenGVLab/InternVL3_5-8B-Flash \
-    --extra-args '{"ignore_eos": true}'
+    --min-prompt-length 280 \
+    --max-prompt-length 280 \
+    --tokenizer-path Qwen/Qwen3-1.7B-FP8 \
+    --extra-args '{"ignore_eos": true}' \
+    --stream
